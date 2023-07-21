0954944d1d537ee70e5b6bdcafe6d253fe4f386c
pranavbadhan8 <pranavbadhan8@gmail.com>
2023-07-21 17:39:07 +0530

    submitting code-Fri Jul 21 2023 17:39:04 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+   let [value,setValue] = useState();
+
+    let clicking = () => 
+    setValue("Modal Content \n This is the modal content")
+
   return (
-    <div>
+    <div className="container">
         {/* Do not remove the main div */}
+        <div className="parent">
+          <h1>Parent Component</h1>
+          <div className="child">
+              <h2>Child Component</h2>
+              <button onClick={clicking}>Show Modal</button>
+              <p>{value}</p>  
+          </div>
+        </div>
     </div>
   )
 }
